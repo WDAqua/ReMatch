57ef1c510629d98b3cd180e8bc566df177bd04a7
Unknown <sakerobs@gmail.com>
2017-07-18 16:27:21 -0700

    api with json

--- api.py
+++ api.py
@@ -2,9 +2,11 @@
 # -*- coding: UTF-8 -*-# enable debugging
 import main
 import cgi, cgitb
+import json
+import numpy as np
 cgitb.enable()   
 print("Content-Type: text/html;charset=utf-8\r\n")
-print("Hello World!") 
+
 
 
 
@@ -12,18 +14,23 @@ print("Hello World!")
 
 form = cgi.FieldStorage()
 question=form.getvalue('question')
-mat, maxLength, glove, patty = main.processPatty()
+mat=np.load('mat.dat')
+maxLength=np.load('maxLength.dat')
+glove = main.load_data('glove.dat')
+patty = main.load_data('patty.dat')
+patty.processData()
 vectors, parts, pos, gen_question, similarities, unweighted, weighted, result = main.processQuestion(glove,maxLength, patty, mat,question )
-print("vectors\r\n")
-print (vectors)
-print("parts\r\n")
-print (parts)
-print("pos\r\n")
-print (pos)
-print("gen_question\r\n")
-print (gen_question)
-print("result\r\n")
-print (result)
+
+vectors_json=json.dumps(vectors[0].tolist())
+o={}
+o["vectors"]=[]
+for vec in vectors:
+	o["vectors"].append(vec.tolist())
+o["parts"]=parts
+o["pos"]=pos
+o["gen_question"]=gen_question
+o["result"]=result
+print (json.dumps(o))
 
 
 
